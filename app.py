e9e6822c482c0e40752b2a8610e2073f9effd53d
derins <ds5bb@virginia.edu>
2020-01-08 16:33:41 -0800

    website allows file upload and converter works, converted file ends up in uploads folder along with the uploaded json

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 
+UPLOAD_FOLDER = './uploads'
+
 app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
 
 @app.route('/about')
 def about():
