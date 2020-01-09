e10eaaaa7e1c6cf9dffc792d8d76486a9bfa90f9
derins <ds5bb@virginia.edu>
2020-01-09 11:26:23 -0800

    converter implemented, form submit converts and downloads the uploaded file

--- app.py
+++ app.py
@@ -13,10 +13,5 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import json
 import sys
 import getopt
 import shutil
+import glob
 from app import app
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['json'])
@@ -23,6 +24,9 @@ def upload_form():
 @app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
+        uploadFiles = glob.glob('./uploads/*')
+        for f in uploadFiles:
+            os.remove(f)
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -32,15 +36,23 @@ def upload_file():
             flash('No file selected for uploading')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            # shutil.rmtree('./uploads/*')
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             flash('File successfully uploaded')
             os.system('python json2glm.py')
-            return redirect('/upload')
+            glmFile = glob.glob('./uploads/*.glm')
+            glmFile = glmFile[0].split("/")[-1:][0]
+            return redirect(url_for('download', filename = glmFile))
         else:
             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
             return redirect('/upload')
+
+
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(directory=uploads, filename=filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
