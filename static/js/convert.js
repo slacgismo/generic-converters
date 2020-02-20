641ebc6f0017def33c7b9f40fa230cdc3d9776dd
Derin Serbetcioglu <serbetd@gmail.com>
2020-02-20 15:55:41 -0800

    going to deendent mapping branch

--- static/js/convert.js
+++ static/js/convert.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function(){
+	$("#convertTo").change(function(){
+		var convertTo = $("#convertTo").val();
+		if (convertTo == "png"){
+			$("#convertForm").append("<div id='json2pngOptions'>Hey</div>")
+		}
+});
